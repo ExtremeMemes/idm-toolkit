493c6b281fd6e906afcfdcdbee3fe0bb0f1f1bf7
Extreme Memes <78830086+ExtremeMemes@users.noreply.github.com>
2022-02-05 13:44:57 -0800

    2.0 release

---
+++ 2.0/toolkit.bat
@@ -0,0 +1,217 @@
+@echo off
+ ::::::::::::::::::::::::::::
+ ::ADMIN PERMS REQUESTING
+ ::::::::::::::::::::::::::::
+ CLS
+ ECHO.
+ ECHO =============================
+ ECHO Running Admin Invocation
+ ECHO =============================
+
+:init
+ setlocal DisableDelayedExpansion
+ set cmdInvoke=1
+ set winSysFolder=System32
+ set "batchPath=%~0"
+ for %%k in (%0) do set batchName=%%~nk
+ set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+ setlocal EnableDelayedExpansion
+
+:checkPrivileges
+  NET FILE 1>NUL 2>NUL
+  if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+
+:getPrivileges
+  if '%1'=='ELEV' (echo ELEV & shift /1 & goto gotPrivileges)
+  ECHO.
+  ECHO **************************************
+  ECHO Invoking UAC for Privilege Escalation
+  ECHO **************************************
+
+  ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+  ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+  ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+  ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+  ECHO Next >> "%vbsGetPrivileges%"
+
+  if '%cmdInvoke%'=='1' goto InvokeCmd 
+
+  ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+  goto ExecElevation
+
+:InvokeCmd
+  ECHO args = "/c """ + "!batchPath!" + """ " + args >> "%vbsGetPrivileges%"
+  ECHO UAC.ShellExecute "%SystemRoot%\%winSysFolder%\cmd.exe", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+
+:ExecElevation
+ "%SystemRoot%\%winSysFolder%\WScript.exe" "%vbsGetPrivileges%" %*
+ exit /B
+
+:gotPrivileges
+ setlocal & cd /d %~dp0
+ if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+
+ ::::::::::::::::::::::::::::
+ ::SCRIPT BEGIN
+ ::::::::::::::::::::::::::::
+
+set support=Current supported versions are 6.40. Do not update after you have patched the Software.
+:: %support%
+
+title github.com/ExtremeMemes/idm-toolkit, version 2.0
+echo ===================================
+echo IDM Toolkit v1.0
+echo This Version Supports IDM 6.40
+echo ===================================
+
+mkdir rescources
+:: Makes Directory
+:start
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+echo .
+
+echo Choose an Option...
+echo Currently Supported Versions: 6.40
+echo 1 - Activate + Patch Latest Version (Installed in C:\Program Files (x86)\Internet Download Manager)
+echo 2 - Activate + Patch Latest Version (Installed in "C:\Program Files\Internet Download Manager")
+echo 3 - Reset Trial + Remove Activation Info (Will not Remove Patch, Reinstall to remove patch)
+echo 4 - Just Activate, dont patch (Will get the error telling that the key is not genuine)
+echo 5 - Install IDM
+echo 6 - Terminate IDM Tasks
+echo 7 - Exit
+
+
+SET /p option=Choose one option - 
+echo .
+echo .
+echo .
+echo .
+echo .
+echo You chose option %option%
+IF %option%==1 GOTO :patch
+
+IF %option%==2 GOTO :patch32
+
+IF %option%==3 GOTO :trial
+
+IF %option%==4 GOTO :activate
+
+IF %option%==5 GOTO :install
+
+IF %option%==6 GOTO :kill
+
+IF %option%==7 GOTO :exit
+
+:: currently not needed, saving for later, first set for local user, and other is for all users
+:: reg add "HKEY_CURRENT_USER\Software\DownloadManager" /v FName /t REG_SZ /d Internet
+:: reg add "HKEY_CURRENT_USER\Software\DownloadManager" /v LName /t REG_SZ /d Download Manager
+:: reg add "HKEY_CURRENT_USER\Software\DownloadManager" /v Email /t REG_SZ /d info@tonec.com
+:: reg add "HKEY_CURRENT_USER\Software\DownloadManager" /v Serial /t REG_SZ /d 9QNBL-L2641-Y7WVE-QEN3I
+
+:: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Internet Download Manager" /v FName /t REG_SZ /d Internet
+:: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Internet Download Manager" /v LName /t REG_SZ /d Download Manager
+:: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Internet Download Manager" /v Email /t REG_SZ /d info@tonec.com
+:: reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Wow6432Node\Internet Download Manager" /v Serial /t REG_SZ /d 9QNBL-L2641-Y7WVE-QEN3I
+
+
+
+:patch
+echo Make sure IDM is closed (EVEN FROM TRAY)
+echo Please also press "Yes" on the popup!
+pause
+echo .
+echo .
+echo %support%
+SET /p pver=Choose a Version - 
+cd rescources
+:: Add curl, example is curl --silent --output https://insert-url.here/file.exe
+curl --silent --output IDMan.exe https://extremememes.github.io/idm-toolkit/res/%pver%/IDMan.exe
+curl --silent --output only-activate.reg https://extremememes.github.io/idm-toolkit/res/only-activate.reg
+ren "C:\Program Files (x86)\Internet Download Manager\IDMan.exe" "IDMan.exe.bak"
+echo If you want to remove the patch, delete IDMan.exe and rename IDMan.exe.bak to IDMan.exe
+xcopy IDMan.exe "C:\Program Files (x86)\Internet Download Manager" /i /q /y
+regedit only-activate.reg
+pause
+del /q IDMan.exe
+del /q only-activate.reg
+cd..
+goto :start
+
+
+:patch32
+echo Make sure IDM is closed (EVEN FROM TRAY)
+echo Please also press "Yes" on the popup!
+pause
+echo .
+echo .
+echo %support%
+SET /p pver32=Choose a Version - 
+cd rescources
+curl --silent --output IDMan.exe https://extremememes.github.io/idm-toolkit/res/%pver32%/IDMan.exe
+curl --silent --output only-activate.reg https://extremememes.github.io/idm-toolkit/res/only-activate.reg
+ren "C:\Program Files\Internet Download Manager\IDMan.exe" "IDMan.exe.bak"
+echo If you want to remove the patch, delete IDMan.exe and rename IDMan.exe.bak to IDMan.exe
+xcopy IDMan.exe "C:\Program Files\Internet Download Manager" /i /q /y
+regedit only-activate.reg
+pause
+del /q IDMan.exe
+del /q only-activate.reg
+cd..
+goto :start
+
+:trial
+echo Make sure IDM is closed (EVEN FROM TRAY)
+echo Please also press "Yes" on the popup!
+pause
+cd rescources
+curl --silent --output idm-regreset.reg https://extremememes.github.io/idm-toolkit/res/idm-regreset.reg
+regedit idm-regreset.reg
+pause > nul
+del /q idm-regreset.reg
+cd..
+goto :start
+
+
+:activate
+echo Make sure IDM is closed (EVEN FROM TRAY)
+echo Please also press "Yes" on the popup!
+pause
+cd rescources
+curl --silent --output only-activate.reg https://extremememes.github.io/idm-toolkit/res/only-activate.reg
+regedit only-activate.reg
+del /q only-activate.reg
+cd..
+goto :start
+
+
+:install
+echo .
+echo .
+echo .
+echo %support%
+SET /p version=Choose a Version - 
+cd rescources
+curl --silent --output idm-install.exe https://extremememes.github.io/idm-toolkit/res/%version%/idm-install.exe
+idm-install.exe
+ping 192.0.2.2 -n 1 -w 5000 > nul
+:: PING IS FOR WAITING FOR 5 SECONDS
+echo Press enter when you have finished the installation. . .
+pause > nul
+del /q idm-install.exe
+cd..
+goto :start
+
+:kill
+echo Press enter to kill the IDM task...
+pause > nul
+TASKKILL /F /IM IDMan.exe
+TASKKILL /F /IM IEMonitor.exe
+goto :start
+:exit
+rmdir /s rescources
+exit
--- README.md
+++ README.md
@@ -1,27 +1,57 @@
-## IDM Toolkit
+# IDM Toolkit
 Download - https://github.com/ExtremeMemes/idm-toolkit/releases/latest
-# Updates for New Versions
+
+#Copyright "Fair Use"
+This has been made for only for educational and research purposes.
+
+*note that the following does not apply to this software, just for reference to the offical law.*
+Copyright Disclaimer under section 107 of the Copyright Act 1976, allowance is made for "fair use" for purposes such as criticism,
+comment, news reporting, teaching, scholarship, education and research.
+Fair use is a use permitted by copyright statute that might otherwise be infringing.
+Non-profit, educational or personal use tips the balance in favor of fair use. 
+ 
+
+# Future
 I will check IDM for updates about every month and will update the patch and post it here.
 
-I am also working for a version with one toolkit for all versions
+**TODO**
+- Make to abort the download if download does not exist (for patch and install)
 
 
-# Features
-- **Activate + Patch Latest Version** - Activates the Key and Patches the IDMan.exe to remove key is not genuine error
-- **Reset Trial + Remove Activation Info** - Removes ALL activation information and Resets your 30 day IDM trial if you are using a trial *THIS WILL NOT REMOVE THE PATCH! TO REMOVE IT, REINSTALL IDM*
-- **Just Activate, dont patch** - You will get the error telling that the key is not genuine
-- **Install IDM** - Just installs the latest version which is bundled in the build in the "recources" folder
-- **Exit** - Closes the window
-
-# Requirements
-  - Need Administrator Permissions
- 
-# Supported IDM Versions
-Version 6.40 build 2 is the only version currently supported. For an older version, request on this google form
-https://forms.gle/7SF23GkUDenGM37L9
-# Credits
-ExtremeMemes https://github.com/ExtremeMemes
 
 # License
 Under the MIT License
 https://github.com/ExtremeMemes/idm-toolkit/blob/main/LICENSE
+
+
+# Don't trust? Dont use
+If you do not trust me, you do not have to use this software
+If you want, you can scan any file on your antivirus or https://virustotal.com/
+
+# Use and Features
+- **Activate + Patch Latest Version** - Activates the Key and Patches the IDMan.exe to remove key is not genuine error
+- **Reset Trial + Remove Activation Info** - Removes ALL activation information and Resets your 30 day IDM trial if you are using a trial
+- **Just Activate, dont patch** - You **WILL** get the error telling that the key is not genuine
+- **Install IDM** - Just installs the chosen IDM version which is curl'd to the recources folder and run for you
+- **Kill IDM** - Closes the IDMan.exe and IEMonitor.exe tasks
+- **Exit** - Closes the window
+
+FOr Activate + Patch/Installing type only the version number, like "6.40"
+
+# Requirements
+  - Need Administrator Permissions
+  - Stable internet connection
+
+# Acknowledgements
+ - You MAY get an error when running the reg files, just try again
+ - If you type a invalid version in Patch/Install, the script will continue with errors
+ - The reset trial has not been tested if it works or not
+ 
+# Supported IDM Versions
+Version 6.40 is the only supported version at the moment. For an older version, or to save a old version, open a issue
+
+# Patch Removal
+To remove the patch, go to the installation directory, delete IDMan.exe, and rename IDMan.bak to IDMan.bak.exe, and it is back to normal
+
+# Credits
+ExtremeMemes https://github.com/ExtremeMemes
